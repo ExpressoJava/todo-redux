a0753885a48212cdccb43705846276afe80ee37a
Bao Pham <ryanvuepham@gmail.com>
2019-02-02 13:18:15 -0600

    working on form.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import './App.css';
 import { connect } from 'react-redux';
 
 class App extends Component {
+  state = {
+    todos: [
+      {id: 1, content: 'buy some milk'}
+      {id: 2, content: "play mario kart"}
+    ]
+  }
   render() {
     return (
       <div className="App">
