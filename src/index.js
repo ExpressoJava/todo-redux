24dfb7231bf32dda678f545d5f4f188d82b975b1
Bao Pham <ryanvuepham@gmail.com>
2019-02-02 12:53:16 -0600

    setting up create store

--- src/index.js
+++ src/index.js
@@ -2,11 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import {createStore} from 'redux';
+import { Provider} from 'react-redux';
+
+const store = createStore(
+    allReducer, 
+    {
+    products: [{name: 'iPhone'}],
+    user: 'Bao'
+    },
+    window.devToolsExtension && window.devToolsExtension() // checking to see if windows has devTools ext. installed and invoke
+); 
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root')
+    );
+    
